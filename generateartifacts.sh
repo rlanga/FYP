133789bdf2679e3e20d466929253dea04782afd4
Roy Langa <roy.langa@gmail.com>
2018-02-06 19:01:46 +0000

    Enabled TLS functionality in all containers

--- generateartifacts.sh
+++ generateartifacts.sh
@@ -3,5 +3,5 @@
 cryptogen generate --config=./crypto-config.yaml
 export FABRIC_CFG_PATH=${PWD}
 configtxgen -profile ECOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
-export CHANNEL_NAME=elections  && configtxgen -profile VoteTallyChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
+export CHANNEL_NAME=elections  && configtxgen -profile VoteTallyChannel -outputCreateChannelTx ./channel-artifacts/electionsChannel.tx -channelID $CHANNEL_NAME
 docker-compose -f docker-compose.yml up -d
